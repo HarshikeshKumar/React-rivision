81ea1415691f035edbaf359838de61edd2b13dd6
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-01-19 15:44:05 +0530

    Practice on 01ReactInPureForm and create new project i.e. HangMan Project and in the HangMan project we create a button component and Passing Props

---
+++ HangManProject/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Button from "./components/Button/Button";
+
+function App() {
+  return (
+    <>
+      <Button text="Click Me" />
+      <Button text="Click Me2" />
+      <Button text="Click Me3" />
+      <Button text="Click Me4" />
+    </>
+  );
+}
+
+export default App;
---
+++ HangManProject/src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+function Button({ text }) {
+  // console.log(props);
+  return (
+    <>
+      <button>{text}</button>
+    </>
+  );
+}
+export default Button;

d0997d5f5204287b3e4bbb6a68bcd17623baf609
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-01-19 19:43:19 +0530

    Created two more components StartGame and TextInput, And Registered TextInput component into App.jsx file

--- HangManProject/src/App.jsx
+++ HangManProject/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Button from "./components/Button/Button";
+import TextInput from "./components/TextInput/TextInput";
 
 function App() {
   return (
@@ -17,6 +18,12 @@ function App() {
         text="Click Me3"
         onClickHandler={() => console.log("Click Me3")}
         styleType="error"
+      />
+
+      <TextInput
+        label="Enter Some text"
+        placeholder="Enter Some Text Here"
+        onChangeHandler={(e) => console.log(e.target.value)}
       />
     </>
   );

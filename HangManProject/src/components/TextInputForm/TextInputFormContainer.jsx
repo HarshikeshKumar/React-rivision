5786567f6e870acaba0ccf1dbd5e5152b87290a0
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-01-21 22:31:30 +0530

    Create one more State variable and set the value of input and access the value of input after Submit the Form

--- HangManProject/src/components/TextInputForm/TextInputFormContainer.jsx
+++ HangManProject/src/components/TextInputForm/TextInputFormContainer.jsx
@@ -2,15 +2,17 @@ import { useState } from "react";
 import TextInputForm from "./TextInputForm";
 
 function TextInputFormContainer() {
-  let [inputType, setInputType] = useState("password");
+  const [inputType, setInputType] = useState("password");
+  const [value, setValue] = useState("");
 
   function handleFormSubmit(event) {
     event.preventDefault();
-    // console.log("Form Submit");
+    console.log("Form Submit", value);
   }
 
   function handleTextInputChange(event) {
     // console.log(event.target.value);
+    setValue(event.target.value);
   }
 
   function handleShowHideClick() {

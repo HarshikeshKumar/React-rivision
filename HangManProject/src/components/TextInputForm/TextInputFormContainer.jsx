3f5606c6bf429a16a1a259e9cf3c6a1b3f54e8d1
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-01-21 23:42:42 +0530

    Implemented Routing In programmetical way using useNavigate() Hook on clicked submit button, If we use useNavigate Hook for routing then we can apply some logic on page routing

--- HangManProject/src/components/TextInputForm/TextInputFormContainer.jsx
+++ HangManProject/src/components/TextInputForm/TextInputFormContainer.jsx
@@ -1,13 +1,24 @@
 import { useState } from "react";
 import TextInputForm from "./TextInputForm";
+import { useNavigate } from "react-router-dom";
 
 function TextInputFormContainer() {
   const [inputType, setInputType] = useState("password");
   const [value, setValue] = useState("");
 
+  const navigate = useNavigate(); // useNavigate is a hook that return a navigate function
+
   function handleFormSubmit(event) {
     event.preventDefault();
     console.log("Form Submit", value);
+    if (value) {
+      // If we have something in value then we want to go to the Play page
+
+      // setTimeout(() => {
+      //   navigate("/play");
+      // }, 5000);
+      navigate("/play");
+    }
   }
 
   function handleTextInputChange(event) {

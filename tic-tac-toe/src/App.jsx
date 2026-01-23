bccefdc70e9241383e3628dbf0369c2a15acccaf
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-01-23 19:08:59 +0530

    Practice done on tic-tac-toe

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Card from "./components/Card/Card";
 import Grid from "./components/Grid/Grid";
 
 function App() {
   return (
     <>
-      <Grid numberOfCards={9} />
+      <Grid noOfCards={9} />
     </>
   );
 }

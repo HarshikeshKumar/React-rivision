0af4f373085152d878486f39ee2fad704196220e
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-01-24 15:43:45 +0530

    Implemented, If data is in downloading process then 'Loading.....', And if data is downloaded then'Data Downloaded'

--- pokedex/src/components/PokemonList/PokemonList.jsx
+++ pokedex/src/components/PokemonList/PokemonList.jsx
@@ -1,19 +1,29 @@
 import { useEffect } from "react";
 import axios from "axios";
 import "./PokemonList.css";
+import { useState } from "react";
 
 function PokemonList() {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   async function downloadPokemons() {
     const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
     // console.log(response);
     console.log(response.data);
+    setIsLoading(false);
   }
 
   useEffect(() => {
     downloadPokemons();
   }, []);
 
-  return <div className="pokemon-list-wrapper">Pokemon List</div>;
+  return (
+    <div className="pokemon-list-wrapper">
+      <div>Pokemon List</div>
+      {isLoading ? "Loading...." : "Data downloaded"}
+    </div>
+  );
 }
 
 export default PokemonList;

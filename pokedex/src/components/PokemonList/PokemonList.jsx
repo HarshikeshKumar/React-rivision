22a44da2bbfe72f92b414862a17bad002c3ff107
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-01-24 16:12:31 +0530

    AXIOS, Successfully Implemented API call using axios

--- pokedex/src/components/PokemonList/PokemonList.jsx
+++ pokedex/src/components/PokemonList/PokemonList.jsx
@@ -10,7 +10,31 @@ function PokemonList() {
   async function downloadPokemons() {
     const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
     // console.log(response);
-    console.log(response.data);
+    // console.log(response.data);
+    const pokemonResults = response.data.results;
+    // console.log(pokemonResults);
+    const pokemonResultPromise = pokemonResults.map((pokemon) =>
+      axios.get(pokemon.url),
+    );
+    // console.log(pokemonResultPromise);
+    const pokemonData = await axios.all(pokemonResultPromise);
+    // console.log(pokemonData);
+
+    const res = pokemonData.map((pokeData) => {
+      const pokemon = pokeData.data;
+      // console.log(pokemon);
+      return {
+        name: pokemon.name,
+        image: pokemon.sprites.other
+          ? pokemon.sprites.other.dream_world.front_default
+          : pokemon.sprites.front_shiny,
+
+        type: pokemon.types,
+      };
+    });
+    console.log(res);
+    setPokemonList(res);
+
     setIsLoading(false);
   }
 

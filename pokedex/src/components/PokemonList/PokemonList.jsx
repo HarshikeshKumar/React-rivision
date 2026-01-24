06757edcc46d8f4e0f4f6b42be30cfc5b81bc099
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-01-24 19:16:17 +0530

    Apply css on Pokedex UI

--- pokedex/src/components/PokemonList/PokemonList.jsx
+++ pokedex/src/components/PokemonList/PokemonList.jsx
@@ -51,12 +51,13 @@ function PokemonList() {
 
   return (
     <div className="pokemon-list-wrapper">
-      <div>Pokemon List</div>
-      {isLoading
-        ? "Loading...."
-        : pokemonList.map((p) => (
-            <Pokemon name={p.name} image={p.image} key={p.id} />
-          ))}
+      <div className="pokemon-wrapper">
+        {isLoading
+          ? "Loading...."
+          : pokemonList.map((p) => (
+              <Pokemon name={p.name} image={p.image} key={p.id} />
+            ))}
+      </div>
     </div>
   );
 }

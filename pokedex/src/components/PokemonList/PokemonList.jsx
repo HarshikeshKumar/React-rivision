a2e8921de5356d35a42b48846704a09d981141c2
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-01-24 14:07:29 +0530

    Understanding useEffect hook, Now the CallBack of useEffect hook are execute at the time of first render of UI

--- pokedex/src/components/PokemonList/PokemonList.jsx
+++ pokedex/src/components/PokemonList/PokemonList.jsx
@@ -1,24 +1,11 @@
-import { useState } from "react";
 import { useEffect } from "react";
 
 function PokemonList() {
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-
   useEffect(() => {
     console.log("Effect Called");
-  }, [x]);
+  }, []);
 
-  return (
-    <>
-      <div>
-        X: {x} <button onClick={() => setX(x + 1)}>Inc</button>
-      </div>
-      <div>
-        Y: {y} <button onClick={() => setY(y + 1)}>Inc</button>
-      </div>
-    </>
-  );
+  return <div>Pokemon List</div>;
 }
 
 export default PokemonList;

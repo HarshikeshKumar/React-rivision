3946fb639d848fe5baac631b3a538915002b22b4
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-01-24 23:08:48 +0530

    Implemented Functionality of PokemonDetails component and also aplly style on PokemonDetails component

--- pokedex/src/components/PokemonDetails/PokemonDetails.jsx
+++ pokedex/src/components/PokemonDetails/PokemonDetails.jsx
@@ -1,8 +1,45 @@
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import { data, useParams } from "react-router-dom";
+import "./PokemonDetails.css";
+
 function PokemonDetails() {
+  const { id } = useParams();
+  // console.log(id);
+  const [pokemon, setPokemon] = useState({ types: [] });
+
+  async function downloadPokemon() {
+    const response = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${id}/`,
+    );
+
+    console.log("Data.....");
+    console.log(response.data);
+    setPokemon({
+      name: response.data.name,
+      image: response.data.sprites.other.dream_world.front_default,
+      weight: response.data.weight,
+      height: response.data.height,
+      types: response.data.types.map((t) => t.type.name),
+    });
+  }
+
+  useEffect(() => {
+    downloadPokemon();
+  }, []);
   return (
-    <>
-      <h1>Pokemon Details page</h1>
-    </>
+    <div className="pokemon-details-wrapper">
+      <div className="pokemon-details-name">Name: {pokemon.name}</div>
+      <img className="pokemon-details-image" src={pokemon.image} />
+      <div className="pokemon-details-weight">Weight: {pokemon.weight}</div>
+      <div className="pokemon-details-height">Hieght: {pokemon.height}</div>
+      <div className="pokemon-details-types">
+        {pokemon.types.map((t) => (
+          <div key={t}>{t}</div>
+        ))}
+      </div>
+    </div>
   );
 }
 

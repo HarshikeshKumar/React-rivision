552ff13d96cf4cbfa356bc9281c30d64fdae8162
HarshikeshKumar <harshikeshkoili214@gmail.com>
2026-01-24 16:46:40 +0530

    ADDED some comments and remove unnacessary logs

--- pokedex/src/components/PokemonList/PokemonList.jsx
+++ pokedex/src/components/PokemonList/PokemonList.jsx
@@ -8,23 +8,27 @@ function PokemonList() {
   const [pokemonList, setPokemonList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  const POKEDEX_URL = "https://pokeapi.co/api/v2/pokemon";
+
   async function downloadPokemons() {
-    const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
-    // console.log(response);
-    // console.log(response.data);
+    // this downloads the list of 20 pokemons
+    const response = await axios.get(POKEDEX_URL);
+
+    // We get the array of pokemons from result
     const pokemonResults = response.data.results;
-    // console.log(pokemonResults);
-    // console.log(response.data);
+
+    // Iterating over the array of pokemons, and using the url, to create
+    // an array of promise that will download those 20 pokemons
     const pokemonResultPromise = pokemonResults.map((pokemon) =>
       axios.get(pokemon.url),
     );
-    // console.log(pokemonResultPromise);
-    const pokemonData = await axios.all(pokemonResultPromise);
-    // console.log(pokemonData);
 
-    const res = pokemonData.map((pokeData) => {
+    // Passing the promise array to axios.all()
+    const pokemonData = await axios.all(pokemonResultPromise); // Array of 20 pokemon detailed data
+
+    // Now iterate on the data of each pokemon, and extract id, name, image,types
+    const pokeListResult = pokemonData.map((pokeData) => {
       const pokemon = pokeData.data;
-      // console.log(pokemon);
       return {
         id: pokemon.id,
         name: pokemon.name,
@@ -35,8 +39,8 @@ function PokemonList() {
         type: pokemon.types,
       };
     });
-    console.log(res);
-    setPokemonList(res);
+    console.log(pokeListResult);
+    setPokemonList(pokeListResult);
 
     setIsLoading(false);
   }
